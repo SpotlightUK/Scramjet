c5bd81d742cc1b94353ac02e5934bf809c8ef47d
dylanbeattie <dylan@dylanbeattie.net>
2016-08-25 16:59:02 +0100

    Added unit tests for entity extensions; ilmerged Newtonsoft.Json during project build.

---
+++ src/Scramjet.CrmPlugins.UnitTests/EntityExtensionTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.Xrm.Sdk;
+using NUnit.Framework;
+using Shouldly;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Scramjet.CrmPlugins.UnitTests {
+    [TestFixture]
+    public class EntityExtensionTests {
+        [Test]
+        public void Flatten_EntityReference() {
+            var er = new EntityReference("contact", Guid.Empty);
+            var s = EntityExtensions.Flatten(er);
+            s.ShouldBe("contact:00000000-0000-0000-0000-000000000000");
+        }
+
+        public static IEnumerable<decimal?> TestMoneyValues {
+            get {
+                yield return 1.0m;
+                yield return 0.0m;
+                yield return -1.0m;
+                yield return 0.000001m;
+                yield return 1.111111m;
+                yield return 123.45m;
+                yield return -123.45m;
+                yield return -0m;
+                yield return 123.4567m;
+                yield return null;
+            }
+        }
+
+
+        [Test]
+        [TestCaseSource(nameof(TestMoneyValues))]
+        public void Flatten_Money(decimal m) {
+            var s = EntityExtensions.Flatten(m);
+            s.ShouldBe(m.ToString(CultureInfo.InvariantCulture));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(Int32.MaxValue)]
+        public void Flatten_OptionSetValue(int v) {
+            var osv = new OptionSetValue(v);
+            EntityExtensions.Flatten(osv).ShouldBe(v.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public static IEnumerable<DateTime?> TestDateValues {
+            get {
+                yield return DateTime.MinValue;
+                yield return DateTime.MaxValue;
+                yield return new DateTime(2016, 3, 23, 16, 13, 15, 447);
+                yield return DateTime.Parse("2016-02-29 23:59:59.999");
+                yield return null;
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestDateValues))]
+        public void Flatten_DateTime_Works(DateTime dt) {
+            EntityExtensions.Flatten(dt).ShouldBe(dt.ToString("O"));
+        }
+    }
+}
--- src/Scramjet.CrmPlugins/NotifyEntityChangePlug.cs
+++ src/Scramjet.CrmPlugins/NotifyEntityChangePlug.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -27,21 +28,111 @@ namespace Scramjet.CrmPlugins {
         }
     }
 
-    //public class ScramjetConfiguration {
-    //    private OrganizationServiceContext context;
-    //    public ScramjetConfiguration(IOrganizationService service) {
-    //        this.context = new OrganizationServiceContext(service);
-    //    }
+    public class Notification {
+        public Notification(IExecutionContext context, Entity editor) {
+            MessageName = context.MessageName ?? "missing_message_name";
+            CrmPluginDepth = context.Depth;
+            CorrelationId = context.CorrelationId;
+            EntityName = context.PrimaryEntityName;
+            EntityId = context.PrimaryEntityId;
+            ChangedBy = (editor == null ? null : editor.ReadUsername());
+            IsIntegrationUser = editor.IsIntegrationUser();
+            ChangedAt = DateTimeOffset.Now;
+        }
 
-    //    const string KEY = "scramjet_configuration_key";
-    //    const string VALUE = "scramjet_configuration_value";
+        /// <summary>The CRM plugin correlationID of the plugin execution that fired this notification</summary>
+        public Guid CorrelationId {
+            get;
+            set;
+        }
 
-    //    public string Setting(string key) {
-    //        var setting = context.CreateQuery("scramjet_configuration")
-    //            .Where(c => c.Attributes.ContainsKey(KEY) && c.Attributes[KEY] == key)
-    //            .Select(c => c.Attributes[VALUE]).FirstOrDefault();
-    //        if (setting == null) throw new KeyNotFoundException(key);
-    //        return ((string)setting);
-    //    }
-    //}
+        /// <summary>The context.Depth of the plugin invocation that fired this notification.</summary>
+        public int CrmPluginDepth {
+            get;
+            set;
+        }
+
+        /// <summary>The EntityLogicalName of the CRM entity - "contact", "account", "salesorder", etc.</summary>
+        public string EntityName {
+            get;
+            set;
+        }
+        /// <summary>The GUID identifying the entity that has been changed.</summary>
+        public Guid EntityId {
+            get;
+            set;
+        }
+
+        /// <summary>The username of the CRM user who made this change.</summary>
+        public string ChangedBy {
+            get;
+            set;
+        }
+
+        /// <summary>Was this change caused by an integration user (normally via the API client), or by a real person using the web UI?</summary>
+        public bool IsIntegrationUser {
+            get;
+            set;
+        }
+        /// <summary>The date/time when the change was submitted.</summary>
+        public DateTimeOffset? ChangedAt { get; set; }
+
+        /// <summary>The MessageName - "Create", "Update", "Delete", etc. - associated with the CRM plugin context that created this message.</summary>
+        public string MessageName { get; set; }
+
+        private Dictionary<string, string> fieldChanges = new Dictionary<string, string>();
+
+        /// <summary>A collection of fields, including the entity ID field and any fields affected by the change.</summary>
+        public Dictionary<string, string> FieldChanges {
+            get {
+                return fieldChanges;
+            }
+            set {
+                fieldChanges = value;
+            }
+        }
+
+        public override string ToString() {
+            var sb = new StringBuilder();
+            sb.AppendLine("entity: " + EntityName);
+            sb.AppendLine("entityid: " + EntityId);
+            sb.AppendLine("change: " + MessageName);
+            sb.AppendLine();
+            foreach (var f in fieldChanges.Where(f => f.Value != null)) {
+                sb.AppendLine(f.Key + " = " + f.Value);
+            }
+            return (sb.ToString());
+        }
+    }
+
+    public static class EntityExtensions {
+        public static bool IsIntegrationUser(this Entity user) {
+            return (user.Attributes.Contains("isintegrationuser") && Convert.ToBoolean(user.Attributes["isintegrationuser"]));
+        }
+
+        public static string ReadUsername(this Entity user) {
+            return (user.Attributes.Contains("domainname") ? (string)user.Attributes["domainname"] : "(username_not_found)");
+        }
+
+        public static Dictionary<string, string> ToFieldChanges(this Entity entity) {
+            return (entity.Attributes.ToDictionary(e => e.Key, e => Flatten(e.Value)));
+        }
+
+        public static string Flatten(object value) {
+            if (value == null)
+                return (null);
+            if (value is EntityReference)
+                return ((EntityReference)value).LogicalName + ":" + ((EntityReference)value).Id;
+            if (value is Money)
+                return ((Money)value).Value.ToString(CultureInfo.InvariantCulture);
+            if (value is OptionSetValue)
+                return ((OptionSetValue)value).Value.ToString(CultureInfo.InvariantCulture);
+            if (value is DateTime)
+                return ((DateTime)value).ToString("O");
+            if (value is DateTimeOffset) {
+                return ((DateTimeOffset)value).ToString("O");
+            }
+            return (value.ToString());
+        }
+    }
 }
